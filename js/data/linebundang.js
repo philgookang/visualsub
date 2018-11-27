aa742bef8b787d6ea97fc6e3709ae558377a1d35
Phil Goo Kang <philgookang@gmail.com>
2018-11-27 23:37:09 +0900

    선 이름변경

--- js/data/linebundang.js
+++ js/data/linebundang.js
@@ -1,8 +1,8 @@
 var data_linebundang = [
-    { "name" : "한티", "line" : "분당선", "x" : 62, "y" : -62 },
-    { "name" : "선정릉", "line" : "분당선", "x" : 62, "y" : -35 },
-    { "name" : "강남구청", "line" : "분당선", "x" : 62, "y" : 7 },
-    { "name" : "압구정로데오", "line" : "분당선", "x" : 62, "y" : 15 },
-    { "name" : "서울숲", "line" : "분당선", "x" : 62, "y" : 38.5 },
-    { "name" : "왕십리", "line" : "분당선", "x" : 63, "y" : 65 }
+    { "name" : "한티", "line" : "bundang", "x" : 62, "y" : -62 },
+    { "name" : "선정릉", "line" : "bundang", "x" : 62, "y" : -35 },
+    { "name" : "강남구청", "line" : "bundang", "x" : 62, "y" : 7 },
+    { "name" : "압구정로데오", "line" : "bundang", "x" : 62, "y" : 15 },
+    { "name" : "서울숲", "line" : "bundang", "x" : 62, "y" : 38.5 },
+    { "name" : "왕십리", "line" : "bundang", "x" : 63, "y" : 65 }
 ];
