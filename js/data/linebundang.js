fd5d13ec2b5aa1e20d14ab7306a0181803e7bf22
Phil Goo Kang <philgookang@gmail.com>
2018-11-28 15:43:23 +0900

    added station id value

--- js/data/linebundang.js
+++ js/data/linebundang.js
@@ -1,8 +1,8 @@
 var data_linebundang = [
-    { "id" : "", "name" : "한티", "line" : "bundang", "x" : 62, "y" : -62 },
-    { "id" : "", "name" : "선정릉", "line" : "bundang", "x" : 62, "y" : -35 },
-    { "id" : "", "name" : "강남구청", "line" : "bundang", "x" : 62, "y" : 7 },
-    { "id" : "", "name" : "압구정로데오", "line" : "bundang", "x" : 62, "y" : 15 },
-    { "id" : "", "name" : "서울숲", "line" : "bundang", "x" : 62, "y" : 38.5 },
-    { "id" : "", "name" : "왕십리", "line" : "bundang", "x" : 63, "y" : 65 }
+    { "id" : "B01", "name" : "한티", "line" : "bundang", "x" : 62, "y" : -62 },
+    { "id" : "B02", "name" : "선정릉", "line" : "bundang", "x" : 62, "y" : -35 },
+    { "id" : "B03", "name" : "강남구청", "line" : "bundang", "x" : 62, "y" : 7 },
+    { "id" : "B04", "name" : "압구정로데오", "line" : "bundang", "x" : 62, "y" : 15 },
+    { "id" : "B05", "name" : "서울숲", "line" : "bundang", "x" : 62, "y" : 38.5 },
+    { "id" : "B06", "name" : "왕십리", "line" : "bundang", "x" : 63, "y" : 65 }
 ];
--- js/data/linejunang.js
+++ js/data/linejunang.js
@@ -1,12 +1,12 @@
 var data_linejunang = [
-    { "id" : "", "name" : "왕십리", "line" : "junang", "x" : 63, "y" : 65 },
-    { "id" : "", "name" : "응봉", "line" : "junang", "x" : 46, "y" : 35 },
-    { "id" : "", "name" : "옥수", "line" : "junang", "x" : 27, "y" : 16.5 },
-    { "id" : "", "name" : "한남", "line" : "junang", "x" : 19.5, "y" : 9.5 },
-    { "id" : "", "name" : "서빙고", "line" : "junang", "x" : 12.5, "y" : 2 },
-    { "id" : "", "name" : "용산", "line" : "junang", "x" : -23, "y" : 7 },
-    { "id" : "", "name" : "효창공원앞", "line" : "junang", "x" : -35, "y" : 27 },
-    { "id" : "", "name" : "공덕", "line" : "junang", "x" : -40, "y" : 33 },
-    { "id" : "", "name" : "서강대", "line" : "junang", "x" : -58.5, "y" : 33 },
-    { "id" : "", "name" : "홍대입구", "line" : "junang", "x" : -77, "y" : 46 }
+    { "id" : "J01", "name" : "왕십리", "line" : "junang", "x" : 63, "y" : 65 },
+    { "id" : "J02", "name" : "응봉", "line" : "junang", "x" : 46, "y" : 35 },
+    { "id" : "J03", "name" : "옥수", "line" : "junang", "x" : 27, "y" : 16.5 },
+    { "id" : "J04", "name" : "한남", "line" : "junang", "x" : 19.5, "y" : 9.5 },
+    { "id" : "J05", "name" : "서빙고", "line" : "junang", "x" : 12.5, "y" : 2 },
+    { "id" : "J06", "name" : "용산", "line" : "junang", "x" : -23, "y" : 7 },
+    { "id" : "J07", "name" : "효창공원앞", "line" : "junang", "x" : -35, "y" : 27 },
+    { "id" : "J08", "name" : "공덕", "line" : "junang", "x" : -40, "y" : 33 },
+    { "id" : "J09", "name" : "서강대", "line" : "junang", "x" : -58.5, "y" : 33 },
+    { "id" : "J10", "name" : "홍대입구", "line" : "junang", "x" : -77, "y" : 46 }
 ];
